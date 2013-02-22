ee599684f5aa23561d87a1eede70302b4457b0f9
NosovDE <NosovDE@gmail.com>
2013-02-22 13:25:30 +0400

    Create project

---
+++ src/test/scala/ru/nde/coursera/FuncSetTest.scala
@@ -0,0 +1,11 @@
+package ru.nde.coursera
+
+/**
+ * Author:      Dmitriy E. Nosov <br>
+ * Date:        22.02.13, 13:24 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+class FuncSetTest {
+
+}
