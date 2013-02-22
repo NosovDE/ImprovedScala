39314c63eac35cbd047484af0ecdec7decc0f101
NosovDE <NosovDE@gmail.com>
2013-02-22 13:22:48 +0400

    Create project

---
+++ src/main/scala/ru/nde/coursera/RecFun.scala
@@ -0,0 +1,13 @@
+package ru.nde.coursera
+
+/**
+ * Author:      Dmitriy E. Nosov <br>
+ * Date:        22.02.13, 13:19 <br>
+ * Company:     Korus Consulting IT<br>
+ * Description:  <br>
+ */
+class RecFun {
+
+
+
+}
